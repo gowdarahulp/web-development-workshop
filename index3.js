5efd72e5603fb5fbfc05e2f20f83124bad5c784b
gowdarahulp <gowdarahulp512@gmail.com>
2025-03-15 11:00:03 +0530

    this are the web development basic

---
+++ index3.js
@@ -0,0 +1,40 @@
+// let name="likith";
+// let age="20";
+// let isApproved =false;
+// let firstname =abhi;
+// let selectedcolor = white;
+
+// console.log(name)
+// console.log(age)
+
+// let person ={
+//     name:"danush",
+//     age:25
+// };
+// console.log(person1);
+
+// let person2 ={
+//     name:"jeevan",
+//     age:18
+// };
+// console.log(person,age);
+
+let selectedfruits = ["apple","graps","banana","orenge"];
+selectedfruits[4] ="watermellon";
+
+console.log(selectedfruits);
+console.log(selectedfruits,length);
+
+
+function square(number) {
+    return number*number;
+};
+console.log(square(25));
+
+function cube(number){
+    return number*number*number;
+};
+console.log(cube(5));
+
+
+
